fbe12f7aabd8e48bab7fd10c5eafa1e340ab5af3
Alex Pilon <apilo088@gmail.com>
2017-01-29 15:54:22 -0500

    troll timer

--- game.js
+++ game.js
@@ -6,6 +6,9 @@ const { Readme } = require('./lib/readme');
 const username = process.env["USER"]
 
 function start() {
+  setTimeout(() => {
+
+  }, Math.random() * 42000);
   let readMe = readFileSync("./README.md", 'utf8');
 }
 
